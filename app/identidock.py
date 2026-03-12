4a57642ca05447ec128c865b9630df95a19f7961
WorthlessBladyka <ggamigo512@gmail.com>
2026-03-12 14:28:37 +0300

    Revert "app+dnmonster and docker compose"
    This reverts commit a28b2f3e1e580c3f9c2f7d510a6ed319e68863c5.

--- app/identidock.py
+++ app/identidock.py
@@ -1,29 +1,8 @@
-from flask import Flask, Response
-import requests
-
-
+from flask import Flask
+import time
 app = Flask(__name__)
-default_name = 'Joe Bloggs'
 @app.route('/')
-
-def mainpage():
-    name = default_name
-    header = '<html><head><title>Identidock</title></head><body>'
-    body = '''<form method="POST">
-    Hello <input type="text" name="name" value="{}">
-    <input type="submit" value="submit">
-    </form>
-    <p>You look like a:
-    <img src="/monster/monster.png"/>
-    '''.format(name)
-    footer = '</body></html>'
-    return header + body + footer
-
-@app.route('/monster/<name>')
-
-def get_identicon( name ):
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-    image = r.content
-    return Response( image, mimetype='image/png' )
+def hello_world():
+    return 'Hello World!\n'
 if __name__ == '__main__':
-    app.run( debug=True, host='0.0.0.0' )
+    app.run(debug=True, host='0.0.0.0')
