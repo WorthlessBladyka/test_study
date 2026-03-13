67870178b21b44025ba5ba596da3a05d0038d183
WorthlessBladyka <ggamigo512@gmail.com>
2026-03-13 14:23:11 +0300

    + redis

--- app/identidock.py
+++ app/identidock.py
@@ -1,12 +1,14 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
+import redis
 
 app = Flask(__name__)
+cache = redis.StrictRedis( host='redis', port=6379, db=0 )
 salt = "UNIQUE_SALT"
 default_name = 'Joe Bloggs'
-@app.route('/', methods=['GET', 'POST'])
 
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
     name = default_name
     name_hash = None
@@ -28,8 +30,13 @@ def mainpage():
 @app.route('/monster/<name>')
 
 def get_identicon( name ):
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-    image = r.content
+    image = cache.get( name )
+    if image is None:
+        print( "Cache miss (промах кэша)", flush=True )
+        r = requests.get( 'http://dnmonster:8080/monster/' + name + '?size=80' )
+        image = r.content
+        cache.set( name, image )
     return Response( image, mimetype='image/png' )
+
 if __name__ == '__main__':
     app.run( debug=True, host='0.0.0.0' )
