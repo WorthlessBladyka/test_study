dcdaba6244257b6e14646fb4b549b1a78503e491
WorthlessBladyka <ggamigo512@gmail.com>
2026-03-14 22:27:16 +0300

    multibuilding + optimization layers

--- exercise1/cmd.sh
+++ exercise1/cmd.sh
@@ -2,9 +2,9 @@
 set -e
 if [ "$ENV" = 'DEV' ]; then
     echo "Running Development Server" # Запуск сервера для разработки
-    exec python "identidock.py"
+    exec python "app/identidock.py"
 else
     echo "Running Production Server" # Запуск сервера для эксплуатации
-    exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+    exec uwsgi --http 0.0.0.0:9090 --wsgi-file app/identidock.py \
     --callable app --stats 0.0.0.0:9191
 fi
