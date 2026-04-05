d2ccc48a200925b1f8f1a2d58e12de9eb0e11627
WorthlessBladyka <ggamigo512@gmail.com>
2026-04-05 17:56:41 +0300

    опциональные параметры + date func + валидация от и до

--- Fastapp/app/main.py
+++ Fastapp/app/main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Optional
+from datetime import date
 
 app = FastAPI()
 
+
 @app.get("/hotels/{hotels_id}")
-def get_hotels(hotels_id: str):
-	return "Отель " + hotels_id
+def get_hotels(
+	location: str,
+	data_in: date,
+	date_out: date,
+	cookie: Optional[bool] = None,
+	stars: Optional[int] = Query(None, ge=1, le=5)
+):
+
+return "Succeses"
